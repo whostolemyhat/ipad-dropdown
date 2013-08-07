d9dfb4f34b4b49309bfa0660499075c4c657e2b3
James Tease <james@jamestease.co.uk>
2013-08-07 16:32:35 +0100

    comments, touch test

--- ipad-dropdown-menu.js
+++ ipad-dropdown-menu.js
@@ -1,24 +1,23 @@
 
-    if(pm.touchDevice) {
-      // show dropdowns on ipad but don't follow top-level links (marinas, berthing) until second click
-      // otherwise can't ever see/click on dropdown
-      function touchClick(e) {
+if('ontouchstart' in document.documentElement) {
+    // show dropdowns on ipad but don't follow top-level links (marinas, berthing) until second click
+    // otherwise can't ever see/click on dropdown
+    function touchClick(e) {
         // stop click
         e.preventDefault();
         // rebind event to other links since the list has changed (removed current link and added old open link)
         $('.primaryWithNav.touch').removeClass('touch').find('> a').bind('click', touchClick);
         // remove the touchClick event from this link and add class to show dropdown
         $(this).unbind('click').parent().addClass('touch');
-      }
-      // bind top-level links
-      $('.primaryWithNav > a').not('.primaryWithNav.touch > a').click(touchClick);
-      // clear dropdown when clicking outside nav
-      $('html').click(function() {
-        $('.primaryWithNav.touch').removeClass('touch').find('> a').bind('click', touchClick);
-      });
-      // make sure links still function but don't hide dropdown
-      $('#mainNav').click(function(e) {
-        e.stopPropagation();
-      });
-
     }
+    // bind top-level links
+    $('.primaryWithNav > a').not('.primaryWithNav.touch > a').click(touchClick);
+    // clear dropdown when clicking outside nav
+    $('html').click(function() {
+        $('.primaryWithNav.touch').removeClass('touch').find('> a').bind('click', touchClick);
+    });
+    // make sure links still function but don't hide dropdown
+    $('#mainNav').click(function(e) {
+        e.stopPropagation();
+    });
+}
